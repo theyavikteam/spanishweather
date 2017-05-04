32989a529677cbf7b563143808563bab2bde65e0
yavik14 <yavik14@gmail.com>
2017-05-04 23:42:25 +0200

    Region List with flags

--- app/src/main/java/com/theyavikteam/spainweather/MyApplication.kt
+++ app/src/main/java/com/theyavikteam/spainweather/MyApplication.kt
@@ -4,15 +4,16 @@ import android.app.Application
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MyApplication:Application(){
+class MyApplication : Application() {
+
+    val aemetEndpoint = "https://opendata.aemet.es/opendata/"
 
     override fun onCreate() {
         super.onCreate()
         val api = Api
-        api.context = this
-        api.retrofit= Retrofit.Builder()
-                .baseUrl("https://opendata.aemet.es/opendata/")
-                .client(SelfSigningClientBuilder.createClient(Api.context, Api.client))
+        api.retrofit = Retrofit.Builder()
+                .baseUrl(aemetEndpoint)
+                .client(SelfSigningClientBuilder.createClient(this, Api.client))
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
     }
